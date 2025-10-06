1e74b2fb690525d71934800a4de73fa4b312edba
Burzor <solomonnduka8@gmail.com>
2025-10-06 15:07:30 +0100

    Delete ani.js

--- ani.js
+++
@@ -1,8 +0,0 @@
-for (let i = 0; i < 10; i++) {
-  const heart = document.createElement('div');
-  heart.classList.add('heart');
-  heart.style.top = `${Math.random() * 100}%`;
-  heart.style.left = `${Math.random() * 100}%`;
-  heart.style.animationDelay = `${Math.random() * 5}s`;
-  document.body.appendChild(heart);
-}
